66ab18cc0bdee273f357a3db0fc60f40e1eb8557
Vula <103462224+vulazon@users.noreply.github.com>
2026-01-22 18:01:54 +0100

    add tick

---
+++ tick.lua
@@ -0,0 +1,33 @@
+-- Controls game updates
+
+local tick = {}
+
+local oldUpdate = Game.update
+
+local tasks = {}
+
+tick.tasks = tasks
+
+function Game:update(dt)
+	oldUpdate(self, dt)
+
+	for f, k in pairs(tasks) do
+		local s, e = pcall(f)
+
+		if not s then
+			error("Tick error: " .. e)
+		end
+	end
+end
+
+function tick:addTask(f)
+	tasks[f] = #tasks + 1
+end
+
+function tick:removeTask(f)
+	if tasks[f] then
+		tasks[f] = nil
+	end
+end
+
+return tick
